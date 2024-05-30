1a86ed110e27aa22383b9a1d25b53f561a7cbf08
jihee Kim <jkim541@myseneca.ca>
2024-05-30 01:04:44 -0400

    Adding Another Route

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage() {
+    return (
+        <main>
+            <h1>Meals Detail</h1>
+        </main>
+    );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,4 @@
-export default function Meals() {
+export default function MealsPage() {
     return (
         <main>
             <h1>Meals</h1>
